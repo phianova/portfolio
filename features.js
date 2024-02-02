32985e8be5dff79db31f7b4ed69e68d3d450d986
phianova <98988580+phianova@users.noreply.github.com>
2024-02-02 13:47:51 +0000

    update projects and fix some styling

--- features.js
+++ features.js
@@ -1,5 +1,6 @@
 // Interactive mobile menu script
     const menuOpenClose = () => {
+        let menuButton = document.getElementById("menu-button");
         let navButton1 = document.getElementById("navlink1");
         let navButton2 = document.getElementById("navlink2");
         let navButton3 = document.getElementById("navlink3");
@@ -7,18 +8,29 @@
         let navButton5 = document.getElementById("navlink5");
         let navButton6 = document.getElementById("navlink6");
         let navButtons = [navButton1, navButton2, navButton3, navButton4, navButton5, navButton6]
-        console.log(navButton1.style.display);
+        let menuShowing = getComputedStyle(menuButton, null).display;
         for (i=0; i<navButtons.length; i++) {
-            console.log(navButtons[i]);
-            console.log(navButtons[i].style.display);
-            if (navButtons[i].style.display === "none" || navButtons[i].style.display === undefined || navButtons[i].style.display === "") {
+            if (menuShowing !== "none" && (navButtons[i].style.display === "none" || navButtons[i].style.display === undefined || navButtons[i].style.display === "")) {
                 navButtons[i].style.display = "inline-flex";
-            } else if (navButtons[i].style.display === "inline-flex") {
+            } else if (menuShowing !== "none" && (navButtons[i].style.display === "inline-flex")) {
                 navButtons[i].style.display = "none";
             }
         }
-
     }
+
+    /*const mobileMenuClose = () => {
+        let menuButton = document.getElementById("menu-button");
+        let navButton1 = document.getElementById("navlink1");
+        let navButton2 = document.getElementById("navlink2");
+        let navButton3 = document.getElementById("navlink3");
+        let navButton4 = document.getElementById("navlink4");
+        let navButton5 = document.getElementById("navlink5");
+        let navButton6 = document.getElementById("navlink6");
+        let navButtons = [navButton1, navButton2, navButton3, navButton4, navButton5, navButton6]
+        if (menuButton.style.display === "inline-flex") {
+
+        }
+    }*/
 
 // Download CV button script
 
